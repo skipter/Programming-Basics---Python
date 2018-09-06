77455cbaff340b981fde106ca1636b83283c416a
Boris Mandzhukov <skipter@abv.bg>
2018-09-07 00:29:56 +0300

    Add BonusScores exercise.

--- SimpleConditions/06.BonusScore.py
+++ SimpleConditions/06.BonusScore.py
@@ -1,17 +1,27 @@
 number = int(input())
-#TODO correct the logic, there is a mistake with calculations.
+# TODO correct the logic, there is a mistake with calculations.
+tempNum = number
+bonus = 0
 if number <= 100:
-    number += 5
-elif number > 100:
+    bonus += 5
+    # number += 5
+elif number > 100 and number < 1000:
     currentNum = number - number * 20 / 100
-    number = currentNum
+    temp = number - currentNum
+    # number += temp
+    bonus += temp
 elif number > 1000:
-    currentNum = number - number * 20 / 100
-    number = currentNum
+    currentNum = number - number * 10 / 100
+    temp = number - currentNum
+    # number += temp
+    bonus += temp
 
-if number % 2 == 0:
-    number += 1
-if number % 10 == 5:
-    number += 2
+if tempNum % 2 == 0:
+    # number += 1
+    bonus += 1
+if tempNum % 10 == 5:
+    # number += 2
+    bonus += 2
 
-print(number)
+print(bonus)
+print(number + bonus)
