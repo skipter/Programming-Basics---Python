538a62171fc7d8f44a6315a33c145074da4a0c0a
Boris Mandzhukov <skipter@abv.bg>
2018-09-15 08:00:18 +0300

    Finish exercises with drawing figures with python.

--- DrawingFiguresWithLoops/13.Stop.py
+++ DrawingFiguresWithLoops/13.Stop.py
@@ -1,9 +1,14 @@
 n = int(input())
 
+f = (n * 2) + (2 * n - 1) - 4
+
 print(f"{'.' * (n + 1)}{'_' * (n * 2 + 1)}{'.' * (n + 1)}")
 
-for x in range(n - 1):
-    print(f"{'.' * (n - x)}//{'_' * (n + 2 + x)}\\\\{'.' * (n - x)}")
-print(f"{'.' * (n - x)}//{'_' * (n * 3)}\\\\{'.' * (n - x)}")
+for x in range(n):
+    print(f"{'.' * (n - x)}//{'_' * ((2 * n - 1) + x + x)}\\\\{'.' * (n - x)}")
 
-#ToDo correct logic
+print(f"//{'_' * (f // 2)}STOP!{'_' * (f // 2)}\\\\")
+
+
+for i in range(n):
+    print(f"{'.' * (i)}\\\\{'_' * ((f + 4) - i - i)}//{'.' * (i)}")
