2fb9309f8a04588169b0480d3cb857077e552f3a
Boris Mandzhukov <skipter@abv.bg>
2018-09-15 08:25:56 +0300

    Add exam preparation section. Correct sections names.

---
+++ Exam Preparation/Flowers.py
@@ -0,0 +1,32 @@
+chrysanthemums = int(input())
+rosses = int(input())
+tulips = int(input())
+season = input()
+holiday = input()
+
+flowersPrice = 0
+
+#ToDO the logic
+
+if season == "Spring" or season == "Summer":
+    chrysanthemumsPrice = 2
+    rossesPrice = 4.10
+    tulipsPrice = 2.50
+    if holiday == "Y":
+        chrysanthemumsPrice =
+        rossesPrice =
+        tulipsPrice =
+elif season == "Autumn" or season == "Winter":
+    chrysanthemumsPrice = 3.75
+    rossesPrice = 4.50
+    tulipsPrice = 4.15
+    if holiday == "Y":
+        chrysanthemumsPrice =
+        rossesPrice =
+        tulipsPrice =
+
+
+
+
+
+print(f"{flowersPrice:.2f}")
